fe7456c7dfb5cc7710a9f6840b87155101bbe49c
AyushWalekar <walekarayush@gmail.com>
2018-04-18 11:59:00 +0530

    Improving performane of websocket

--- project/server.py
+++ project/server.py
@@ -11,13 +11,24 @@ import dlib
 from webcam import start
 import socket
 from multiprocessing.pool import ThreadPool
+from math import floor, sqrt, ceil
+import pickle
 
 # initialize dlib's face detector
 # print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+count = 0
+
+# initializing array to store previous frames
 prev_shape = []
+
+# setting multirocessing limit as 25
 pool = ThreadPool(processes=25)
+
+# finale frame and frame on which panda will be drawn initially
+blank_image = np.ones((650, 650, 3), np.uint8)*255
+drawing = np.ones((650, 650, 3), np.uint8)*255
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -34,28 +45,35 @@ class SimpleWebSocket(tornado.websocket.WebSocketHandler):
     async def on_message(self, text_data):
 
         global prev_shape
-        text_data_json = json.loads(text_data)
+        image = np.frombuffer(text_data, dtype=np.uint8)
+        image = np.reshape(image, (600, 600, 4))
 
-        message = text_data_json['message'][22:]
-        imgdata = base64.b64decode(message)
+        # converted image from RGB-A to RGB
+        img = image[:, :, :-1]
 
-        image = Image.open(BytesIO(imgdata))
-        img = np.array(image)[:, :, :-1]
+        # changing RGB to BGR (OpenCV uses BGR formatting)
         img = img[:, :, ::-1]
 
+        # resizing to appropriate format and inverting it
         frame = cv2.resize(img, (600, 600), interpolation=cv2.INTER_CUBIC)
         frame = cv2.flip(frame, 1)
 
-        async_result = pool.apply_async(start, (frame, detector, predictor, prev_shape))
-        frame, prev_shape = async_result.get()
+        try:
+            "# using multiprocessing to decrease delay in stream"
+            async_result = pool.apply_async(start, (frame, detector, predictor, prev_shape, blank_image, drawing))
+        
+            frame, prev_shape = async_result.get()
 
-        frame = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_LINEAR)
-        retval, frame = cv2.imencode('.jpg', frame)
-        message = str(base64.b64encode(frame))[2:-1]
+            "# converting numpy array back to base64"
+            retval, frame = cv2.imencode('.jpg', frame)
+            message = str(base64.b64encode(frame))[2:-1]
 
-        [await client.write_message(json.dumps({
-            'message': message,
-        })) for client in self.connections]
+            await self.write_message(json.dumps({
+                'message': message,
+            }))
+        except:
+            await self.write_message(json.dumps({'message': 'error'}))
+            #print("popup ana chahye")
 
     def on_close(self):
         self.connections.remove(self)
@@ -64,6 +82,8 @@ class SimpleWebSocket(tornado.websocket.WebSocketHandler):
 def make_app():
 
     return tornado.web.Application([
+        (r"/static/(.*)", tornado.web.StaticFileHandler, {'path': 'static/'}),
+        (r"/static/css/(.*)", tornado.web.StaticFileHandler, {'path': 'static/'}),
         (r"/", MainHandler),
         (r"/websocket", SimpleWebSocket)
     ], autoreload=True)
@@ -73,8 +93,13 @@ if __name__ == "__main__":
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
     sock.setblocking(0)
-    sockets = tornado.netutil.bind_sockets(8001)
+    sockets = tornado.netutil.bind_sockets(8000)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server = tornado.httpserver.HTTPServer(make_app())
     server.add_sockets(sockets)
     tornado.ioloop.IOLoop.current().start()
+    '''
+    except:
+            #print("none type aiiii")
+            await self.write_message(json.dumps({'message': 'error'}))
+    '''
---
+++ project/static/js/websocket.js
@@ -0,0 +1,111 @@
+
+//const  start_button = document.getElementById('start');
+var img = document.getElementById('image');
+var video = document.getElementById('camera');
+
+var canvas = document.getElementById('canvas');
+
+navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+
+//socket initialization
+var chatSocket = new WebSocket(
+  'ws://' + window.location.host +
+  '/websocket');
+
+chatSocket.binaryType = 'arraybuffer';
+chatSocket.bufferedAmount = 0;
+
+var show_popup = false;
+var popup = document.getElementById("myPopup");
+//server response
+chatSocket.onmessage = function (e) {
+
+  // Json recieved
+  //console.log("Json recieved");
+  var data = JSON.parse(e.data);
+  var message = data['message'];
+  //console.log(message);
+  if (message === 'error') {
+    console.log("pop ana chayhye");
+    // alert("cant detect face");
+    if (!show_popup) {
+      popup.classList.toggle("show");
+      show_popup = true;
+    }
+  }
+  else {
+    img.src = 'data:image/png;base64,' + message;
+    if (show_popup) {
+      popup.classList.toggle("show");
+      show_popup = false;
+    }
+  }
+};
+
+/*
+  function Base64Encode(str, encoding = 'utf-8') {
+    console.log("encodeing image");
+    var bytes = new (TextEncoder || TextEncoderLite)(encoding).encode(str);
+    return base64js.fromByteArray(bytes);
+  }
+*/
+chatSocket.onclose = function (e) {
+  console.error('Chat socket closed unexpectedly');
+};
+
+function start_clicked() {
+  navigator.getMedia(
+    // constraints
+    { video: true, audio: false },
+
+    // success callback
+    function (localMediaStreamTrack) {
+      var video = document.getElementsByTagName('video')[0];
+      video.srcObject = localMediaStreamTrack;
+      video.play();
+      //console.log("video width" + video.width);
+    },
+
+    //handle error
+    function (error) {
+
+      document.write("Webcam not working");
+      console.log(error);
+    });
+
+    setInterval(capture_frame, 320);
+    //capture_frame();
+}
+
+//var context = canvas.getContext('2d');
+
+var count = 0;
+function capture_frame() {
+  count = count + 1;
+  if (count === 1 || count > 15) {
+    canvas.width = video.width;
+    canvas.height = video.height;
+    ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0);
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var typedArray = imageData.data // data is a Uint8ClampedArray
+
+    chatSocket.send(typedArray);
+  }
+}
+
+//play pause button
+function play_pause_func() {
+
+  var play_pause = document.getElementById("play_pause");
+  if (video.paused) {
+    video.play();
+    play_pause.innerText = "Pause";
+    play_pause.style.backgroundColor = "Red";
+  }
+  else {
+    video.pause();
+    play_pause.innerText = "Play";
+    play_pause.style.backgroundColor = "White";
+  }
+}
